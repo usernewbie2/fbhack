9f3e524f2ec78b2787ba35adb1a6259ca478b06f
usernewbie2 <51822311+usernewbie2@users.noreply.github.com>
2019-06-28 13:00:37 +0700

    Add files via upload

---
+++ tutorial.py
@@ -0,0 +1,120 @@
+#!/usr/bin/env python
+#prank tools
+#kesian amad
+
+import os 
+import sys
+import time 
+
+os.system('clear')
+
+def runntek(s):
+        for c in s + '\n':
+                sys.stdout.write(c)
+                sys.stdout.flush()
+                time.sleep(10. / 100)
+
+if sys.platform == "linux" or sys.platform == "linux2":
+     GL = "\033[96;1m" # Blue aqua
+     BB = "\033[34;1m" # Blue light
+     YY = "\033[33;1m" # Yellow light
+     GG = "\033[32;1m"  # Green Light
+     RR = "\033[31;1m" # Red light
+     CC = "\033[36;1m" # Cyan light
+     B = "\033[34m"    # Blue
+     Y = "\033[33;1m"    # Yellow
+     G = "\033[32m"    # Green
+     W = "\033[0;1m"     # White
+     R = "\033[31m"    # Red
+     C = "\033[36;1m"    # Cyan
+print ()
+
+def cover():
+    print (""
+
+
+            "")
+    runntek(GL+"             Assalamu'alaikum.  Gaess")
+    time.sleep(1)
+    print (" ")
+    print (GG+"  +============================================+")
+    print (RR+"  +   **********  HACK FACEBOOK  ***********   +")
+    print (GG+"  +============================================+")
+    print (GL+"  +              MOD BY : Ramadhan               +")
+    print (YY+"  +             BACALAH BASMALLAH              +")
+    print (B+ "  +         TAK ADA YG TIDAK MUNGKIN           +")
+    print (Y+ "  +             SEMUA BUTUH PROSES             +")
+    print (G+ "  +--------------------------------------------+")
+    print (C+ "  +   ********  LIFE OF PROGRAMER  *********   +")
+    print (R+ "  +--------------------------------------------+")
+    print (GG+"  +============================================+")
+    print (RR+"  +   **********  HACK FACEBOOK  ***********   +")
+    print (GG+"  +============================================+")
+    time.sleep(3)
+
+cover()
+
+#variable
+
+nama = ("       \033[96;1m SELAMAT DATANG DITOOLS FB HACK")
+print (nama)
+print ("")
+
+#input ouput
+
+time.sleep (3)
+print (R+" ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^")
+halo = (GG+"       ^^^^^  SILAHKAN LOGIN DULU ^^^^^^")
+print (halo)
+time.sleep (3)
+print (C+" ================================")
+email = input(GG+" Masukan Username: ")
+
+
+print ("")
+psw = input(GG+" Masukan password: ")
+
+login = 'https://www.facebook.com/login.php?login_attemp=1'
+
+#hasil 
+
+os.system("clear")
+os.system("figlet Loadjing")
+print ("[                    ] 0% ")
+time.sleep(5)
+print ("[=====               ] 25%")
+time.sleep(5)
+print ("[==========          ] 50%")
+time.sleep(5)
+print ("[===============     ] 75%")
+time.sleep(5)
+print ("[====================] 100%")
+time.sleep (3)
+print (" ===============================")
+yap = (GL+" •••••• Login Berhasil Bosq ••••••") 
+print (yap)
+
+#akhir
+
+print ("")
+time.sleep 
+runntek ("        ===== SORRY DATA LU GW AMBIL:V =======")
+print ("")
+
+time.sleep(3)
+runntek ("        ===== TAPI BOONG HAHAHAHAHA =======")
+print ("")
+
+time.sleep(3)
+runntek ("        ===== UDAH TAU INI TOOLS BOONGAN =====")
+print ("")
+
+time.sleep(3)
+runntek ("        ===== MENDING TIDUR BESOK MULUNG ====")
+print ("")
+
+time.sleep(3)
+os.system("figlet SEMUA AKAN INDAH PADA WAKTUNYA")
+
+
+
